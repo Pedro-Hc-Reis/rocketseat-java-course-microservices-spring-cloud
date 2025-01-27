adacffa6ea87106ad52ab49a14707bd5358bfab2
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2025-01-27 10:55:27 -0300

    aula_07

--- src/main/java/br/com/rocketseat/service_main/ServiceMainApplication.java
+++ src/main/java/br/com/rocketseat/service_main/ServiceMainApplication.java
@@ -2,6 +2,8 @@ package br.com.rocketseat.service_main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
